a58e8172bc579722b590b6d20343018c35a53b04
sshivahh <russelshivah16@gmail.com>
2024-05-04 21:34:13 +0700

    update page about
    buat card untuk subjects di page about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,26 +4,25 @@
     import whiteLayer1 from "$lib/assets/curves/white_layer1.svg"
     import blueLayer1 from "$lib/assets/curves/blue_layer1.svg"
     import blueLayer2 from "$lib/assets/curves/blue_layer2.svg"
+    
+
+    import Card from "../components/Card.svelte";
 </script>
 
 <Layout active="About">
-    <section class="h-[700px] bg-white border-4 border-red-500 flex flex-wrap justify-center gap-[18vw]">
-        <div class="w-fit font-semibold text-8xl text-MainBlue text-center my-auto leading-[80px] ">
+    <section class="h-[700px] bg-white flex flex-col justify-center items-center">
+        <div class="w-fit fel font-semibold text-8xl text-MainBlue text-center my-auto leading-[80px] ">
             <h1>What</h1>
             <h1>We</h1>
             <h1>Teach</h1>
         </div>
-        <div class="border-green-300 border-4 w-[500px] text-MainBlue font-semibold text-4xl text-center my-auto">
-            <ul class="">
-                <li>Mandarin</li>
-                <li>Physics</li>
-                <li>Math</li>
-                <li>Chemistry</li>
-                <li>Biology</li>
-                <li>Civics</li>
-                <li>Religion</li>
-                <li>And others!</li>
-            </ul>
+        <div class="max-w-[70vw] gap-[2vw] flex-wrap justify-center text-MainBlue font-semibold z-10 text-4xl text-center my-auto flex">
+                <Card subject="Mandarin"></Card>
+                <Card subject="Math"></Card>
+                <Card subject="Physics"></Card>
+                <Card subject="Biology"></Card>
+                <Card subject="Chemistry"></Card>
+                <Card subject="More..."></Card>
         </div>
     </section>
     <div class="relative h-48 m-0 p-0">
---
+++ src/routes/components/Card.svelte
@@ -0,0 +1,34 @@
+<script>
+    import button_wave from "$lib/assets/curves/button_wave.svg"
+
+    export let subject="halo";
+</script>
+
+<div class="bg-white border-[6px] transition-all relative duration-300 border-MainBlue rounded-full w-fit h-fit py-2 px-4 hover:bg-MainBlue">
+    <p class="text-MainBlue">{subject}</p>
+    <div class="wave"></div>
+</div>
+
+<style>
+
+    div:hover > p {
+        transition: 300ms;
+        color: white;
+    }
+
+
+</style>
+<!-- <div class="bg-MainBlue border-[6px] transition-all relative duration-300 border-MainBlue rounded-full w-fit h-fit py-2 px-4 hover:bg-white">
+    <p class="text-white">{subject}</p>
+    <div class="wave"></div>
+</div>
+
+<style>
+
+    div:hover > p {
+        transition: 300ms;
+        color: #91d8f4;
+    }
+
+
+</style> -->
